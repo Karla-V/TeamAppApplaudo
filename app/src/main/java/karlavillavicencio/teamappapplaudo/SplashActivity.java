d68ef82d8c77075b37c310dcde6b708b6bc29a0e
Karla-V <susume07@gmail.com>
2017-11-30 23:12:48 -0600

    creacion de splash screen

--- app/src/main/java/karlavillavicencio/teamappapplaudo/SplashActivity.java
+++ app/src/main/java/karlavillavicencio/teamappapplaudo/SplashActivity.java
@@ -2,12 +2,22 @@ package karlavillavicencio.teamappapplaudo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private int splashTime=1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, TeamActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, splashTime);
     }
 }
