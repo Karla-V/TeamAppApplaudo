583aea6a5950b8f65ba1a8c6fb21c4cc2d0f59f8
Karla-V <susume07@gmail.com>
2017-12-02 00:35:51 -0600

    creacion de la lista con imagenes

--- app/src/main/java/karlavillavicencio/teamappapplaudo/TeamActivity.java
+++ app/src/main/java/karlavillavicencio/teamappapplaudo/TeamActivity.java
@@ -2,12 +2,40 @@ package karlavillavicencio.teamappapplaudo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class TeamActivity extends AppCompatActivity {
+    private String team[]=new String[]{"Arsenal","Liverpool","ManchesterCity","Chelsea","ManchesterUnited", "Crystal Palace","RealMadrid"};
+
+    private Integer image[] = {
+            R.drawable.arsenal,
+            R.drawable.liverpool,
+            R.drawable.manchester_city,
+            R.drawable.chelsea,
+            R.drawable.manchester_united,
+            R.drawable.crystal_palace,
+            R.drawable.real_madrid
+    };
+
+    private ListView teamList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
+
+        TeamListAdapter adapter=new TeamListAdapter(this,team,image);
+        teamList=(ListView)findViewById(R.id.teamList);
+        teamList.setAdapter(adapter);
+        teamList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem=team[+position];
+                Toast.makeText(getApplicationContext(), selectedItem, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/karlavillavicencio/teamappapplaudo/TeamListAdapter.java
@@ -0,0 +1,40 @@
+package karlavillavicencio.teamappapplaudo;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+/**
+ * Created by Karla Villavicencio on 1/12/2017.
+ */
+
+public class TeamListAdapter extends ArrayAdapter<String> {
+    private final Activity context; //activity que contiene la lista
+    private final String[] team;    //lista de equipos
+    private final Integer[] image; // logotipos de equipo
+
+    public TeamListAdapter(Activity context, String[]team, Integer[] image) {
+        super(context, R.layout.list_element, team);
+        this.context=context;
+        this.team=team;
+        this.image=image;
+    }
+
+    public View getView(int posicion,View view, ViewGroup parent){
+
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.list_element,null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.teamName);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
+        TextView txtDescripcion = (TextView) rowView.findViewById(R.id.teamAddress);
+
+        txtTitle.setText(team[posicion]);
+        imageView.setImageResource(image[posicion]);
+        txtDescripcion.setText(team[posicion]);
+
+        return rowView;
+    }
+}
